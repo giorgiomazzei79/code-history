79ec9154ef995e16ffdf5729f7168cf49984a3cd
Giorgio Mazzei <giorgio@fxdigital.uk>
2022-07-29 02:04:27 +0200

    Initial Commit

---
+++ code-history/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  code-history
+//
+//  Created by Giorgio Mazzei on 29/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ code-history/code_historyApp.swift
@@ -0,0 +1,17 @@
+//
+//  code_historyApp.swift
+//  code-history
+//
+//  Created by Giorgio Mazzei on 29/07/22.
+//
+
+import SwiftUI
+
+@main
+struct code_historyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
